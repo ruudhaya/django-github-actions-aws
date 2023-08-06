25858efdaf099cde82c821171e33b2ba849a57e6
Udhay <udhayapsg@gmail.com>
2023-08-07 00:11:17 +0530

    updated test to verfy 404 instead of 200

--- core/tests.py
+++ core/tests.py
@@ -4,4 +4,4 @@ from django.test import TestCase
 class ViewsTestCase(TestCase):
     def test_home_page_loads(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
